d36ff85a846cacd8d6dc914cd9245748ca74b2ce
Rob Loach <robloach@gmail.com>
2014-10-20 12:43:13 -0400

    Add HTMLHint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 var gulp = require('gulp');
 
 // Deploy to GitHub Pages
-
 gulp.task('deploy', function () {
 	var deploy = require("gulp-gh-pages");
     gulp.src("./out/**/*")
         .pipe(deploy());
 });
+
+// HTML Hint
+gulp.task('htmlhint', function () {
+	var htmlhint = require("gulp-htmlhint");
+	gulp.src(["./out/*.html"])
+		.pipe(htmlhint())
+		.pipe(htmlhint.reporter())
+		.pipe(htmlhint.failReporter())
+});
+
+// Test
+gulp.task('test', ['htmlhint']);
+
+// Default
+gulp.task('default', ['test']);
